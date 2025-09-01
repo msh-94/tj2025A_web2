6400312bd010d4e8367b1615395d24239f9f42e3
msh-94 <alstjdgh2003@naver.com>
2025-09-01 11:18:03 +0900

    spring 19일차 멀티쓰레드 설정

--- src/main/java/example/day01/_1스프링스레드/ThreadService.java
+++ src/main/java/example/day01/_1스프링스레드/ThreadService.java
@@ -11,6 +11,7 @@ public class ThreadService { // class start
         int result = 0; // 값 저장 변수
         for (int i = 1; i < 11; i++){ // 반복문
             result += i;
+            System.out.println("ThreadService.thread1");
             // ** 만약에 서비스 처리가 늦어진다면 반환?? **
             // 예시] 현재 스레드 1초간 일시정지 , *10 -> HTTP 10초후 응답
             try { Thread.sleep(1000);
@@ -20,11 +21,12 @@ public class ThreadService { // class start
     }// func end
 
     // 2.
-    @Async
+    @Async // 응답 먼저하고 내부적으로 처리하는 상황
     public void thread2(){
         int result = 0;
         for (int i = 1; i < 11; i++){
             result += i;
+            System.out.println("ThreadService.thread2");
             try{ Thread.sleep(1000);
             }catch (Exception e){ System.out.println(e); }
         }// for end
