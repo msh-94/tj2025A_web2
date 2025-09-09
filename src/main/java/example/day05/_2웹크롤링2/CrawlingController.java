7d68a307116cfd93b51c28a3a1d52deab504516d
msh-94 <alstjdgh2003@naver.com>
2025-09-09 13:25:30 +0900

    spring 23일차 셀레니움 웹크롤링 무한스크롤링

--- src/main/java/example/day05/_2웹크롤링2/CrawlingController.java
+++ src/main/java/example/day05/_2웹크롤링2/CrawlingController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,9 +14,15 @@ import java.util.Map;
 public class CrawlingController { // class start
     // @Autowired : DI 스프링 컨테이너에서 빈(객체) 꺼내기
     private final CrawlingService crawlingService;
-
+    // 1.
     @GetMapping("/crawling1")
     public Map<String,String> task1(){
         return crawlingService.task1();
+    }// func end
+
+    // 2.
+    @GetMapping("/crawling2")
+    public List<String> task2(){
+        return crawlingService.task2();
     }// func end
 } // class end
