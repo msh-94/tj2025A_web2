7ff4fd00d1ddbfb07111f1f718310514f492f4c1
msh-94 <alstjdgh2003@naver.com>
2025-09-16 10:20:01 +0900

    리액트 1일차 컴포넌트

--- src/main/reactapp/src/App.jsx
+++ src/main/reactapp/src/App.jsx
@@ -1,11 +1,15 @@
 
-
+// 함수 == 컴포넌트 == 새로운 마크업 만들기
 function App() {
+  // js code start
+  // ~~~~~~
+  // js code end
   return (
     <>
       <h1> 여기가 최초 렌더링 되는 컴포넌트(화면 그리는 함수) 입니다. </h1>
     </>
   )
-}
+}// func end
 
+// 다른 js파일에서 함수를 import 할수 있도록 export
 export default App
--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -17,11 +17,16 @@ import { createRoot } from 'react-dom/client';
 const root = document.querySelector('#root');
 // 3. 가져온 root 마크업에 렌더링할 객체 생성
 const create = createRoot(root);
+
 // 4. root에 렌더링할 컴포넌트(화면함수)
   // 4-1 렌더링하기 할 컴포넌트 (함수) 가져오기
-import App from './App.jsx';
-  // 4-2 렌더링하기
-create.render( <App /> );
+// import App from './App.jsx';
+// 4-2 렌더링하기k
+// create.render( <App /> );
 
 // * 2 ~ 4-2 요약가능
 // createRoot( document.querySelector('#root').render( <App /> ));
+
+// day01
+import Component1 from './example/day01/Component1.jsx';
+create.render( <Component1/> );
