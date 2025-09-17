3e7a43047b331792771480bccc928f9e8f1e8a09
msh-94 <alstjdgh2003@naver.com>
2025-09-17 14:54:35 +0900

    react 2일차 useState 실습3

--- src/main/reactapp/src/example/day02/Task3.jsx
+++ src/main/reactapp/src/example/day02/Task3.jsx
@@ -5,6 +5,7 @@ export default function Task3( props ){
     const [ stock , setStock ] = useState(0);
     return (
         <>
+        <h3> Task3 </h3>
         제품명 : <input value={ data } onChange={ (e) => { setData( e.target.value ); } }/>
         <h4> 현재 수량 : { stock } </h4>
         <button onClick={ () => { setStock( stock - 1 ); }}> 감소 </button>
