f922cc73e5198ee494e8a48b98f4ac20dff6c67a
msh-94 <alstjdgh2003@naver.com>
2025-09-24 10:25:18 +0900

    퍼시스턴스 설정

--- src/main/reactapp/src/example/day05/실습7/store/store.jsx
+++ src/main/reactapp/src/example/day05/실습7/store/store.jsx
@@ -6,13 +6,28 @@ import userSlice from './userStore';
 */
 /* 
     퍼시스턴스(사이드렌더링) : 로컬/세션 스토리지 에 상태 유지하는 방법
-    설치 : npm i redux-persist
+    1. 설치 : npm i redux-persist
+    2. 스토리지 설정
 */
+// [4] redux-persist 설정
+import storage from 'redux-persist/lib/storage';
+// const persistConfig = { ket : 'key이름' , storage }
+const persistConfig = { ket : 'user' , storage }
+
+// [5] 리듀서에 persist 설정
+import { persistStore , persistReducer } from 'redux-persist';
+// const persistedReducer = persistedReducer( 옵션 , 설정할리듀서 );
+const persistedReducer = persistedReducer( persistConfig , userSlice );
+
 // [1] 스토어 생성
 const store = configureStore( {
     reducer : {
-        user : userSlice
+        // user : userSlice // 퍼시스턴스 *적용전*
+        // [6] 퍼시스턴스 적용된 리듀서를 스토어 등록
+        user : persistedReducer // 퍼시스턴스 *적용후*
     }
 });
 // [2] 내보내기
 export default store;
+// [7] 등록된 퍼시스턴스 스토어 내보내기
+export const persistor = persistStore( store );
