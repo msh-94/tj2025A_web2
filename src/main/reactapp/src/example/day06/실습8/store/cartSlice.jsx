a763a142e3d8984e64d563d5dd1753759792f3f1
msh-94 <alstjdgh2003@naver.com>
2025-09-25 10:42:24 +0900

    실습8 find함수 대신 for문 사용법

--- src/main/reactapp/src/example/day06/실습8/store/cartSlice.jsx
+++ src/main/reactapp/src/example/day06/실습8/store/cartSlice.jsx
@@ -8,12 +8,21 @@ const cartSlice = createSlice( {
     reducers : {
         countAdd : ( state , action ) => {
             state.count += 1
-            const cartList = state.cartInfo.find(c => c.id === action.payload.id);
-            if (cartList) {
-                cartList.amount += 1;
-            } else {
-                state.cartInfo.push({ ...action.payload, amount: 1 });
-            }// if end
+
+            // 방법1
+            // const cartList = state.cartInfo.find(c => c.id === action.payload.id);
+            // if (cartList) {
+            //     cartList.amount += 1;
+            // } else {
+            //     state.cartInfo.push({ ...action.payload, amount : 1 });
+            // }// if end
+
+            // 방법2
+            const found = false;
+            state.cartInfo.forEach( (c) => {
+                if(c.id == action.payload.id){ c.amount += 1; found = true; }
+            })
+            if(!found){ state.cartInfo.push({...action.payload , amount : 1 })}
         }
     }
 });
