fe781160ad186548393fd51fb6656128020262f5
msh-94 <alstjdgh2003@naver.com>
2025-09-25 11:01:54 +0900

    실습8

--- src/main/reactapp/src/example/day06/실습8/store/store.jsx
+++ src/main/reactapp/src/example/day06/실습8/store/store.jsx
@@ -2,13 +2,13 @@ import { persistStore , persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import cartSlice from './cartSlice';
 import { configureStore } from '@reduxjs/toolkit';
+
 const persistConfig = { key : 'cart' , storage }
 const persistedReducer = persistReducer( persistConfig , cartSlice );
 
 const store = configureStore( {
-    reducer : {
-        cart : persistedReducer
-    }
+    reducer : { cart : persistedReducer }
 })
+
 export default store;
 export const per = persistStore( store );
