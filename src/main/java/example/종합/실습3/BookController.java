85c6d86f2f7bf7eae5dbca75c83fbe767ee4ac75
msh-94 <alstjdgh2003@naver.com>
2025-09-26 13:05:14 +0900

    컨트롤러에서 런타임 오류 예외처리

--- src/main/java/example/종합/실습3/BookController.java
+++ src/main/java/example/종합/실습3/BookController.java
@@ -14,14 +14,26 @@ public class BookController { // class start
     // [1] 대출
     @PostMapping("/rent")
     public ResponseEntity<Boolean> bookRent(@RequestBody RentalsDto dto){
-        boolean result = bookService.bookRent(dto);
-        return ResponseEntity.ok().body(result);
+        boolean result = false;
+        try {
+            result = bookService.bookRent(dto);
+            // 만약에 커밋이면
+            return ResponseEntity.ok().body(result);
+        } catch (RuntimeException e) {
+            // 만약에 롤백이면
+            return ResponseEntity.status(405).body(result);
+        }// try catch end
     }// func end
 
     // [2] 반납
     @PostMapping("/return")
     public ResponseEntity<Boolean> bookReturn(@RequestBody RentalsDto dto){
-        boolean result = bookService.bookReturn(dto);
-        return ResponseEntity.ok().body(result);
+        boolean result = false;
+        try {
+            result = bookService.bookReturn(dto);
+            return ResponseEntity.ok().body(result);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(405).body(result);
+        }// try catch end
     }// func end
 }// class end
