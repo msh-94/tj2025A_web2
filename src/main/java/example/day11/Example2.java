98e180c8d6ad30ebd5ebaaa994cbd3e81fb6795c
msh-94 <alstjdgh2003@naver.com>
2025-09-29 12:22:54 +0900

    람다표현식 사용하는 함수형 인터페이스들 + stream

---
+++ src/main/java/example/day11/Example2.java
@@ -0,0 +1,24 @@
+package example.day11;// 패키지명
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example2 { // class start
+    public static void main(String[] args) { // main start
+        // 임의
+        List<Integer> numbers = List.of( 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10 );
+
+        // 스트림이란? 데이터가 다니는 통로
+        // 스트림 API : 데이터(매개변수) ---> 중간연산 ---> 최종출력
+
+        // [1] stream() + forEach() : 중간연산 없이 최종출력
+        // 매개변수에 반복변수를 *하나씩* 대입하여 return 없는 반복문
+        numbers.stream().forEach( x -> System.out.println("[1] forEach : " + x ));
+
+        // [2] stream() + map() + collect( Collectors.toXXX() ) : 중간연산(MAP) 최종출력(collect)
+        // 매개변수에 반복변수를 *하나씩* 대입하여 return 있는 반복문
+        List<Integer> newNumbers =
+            numbers.stream().map( x -> x * 2 ).collect( Collectors.toList() );
+        System.out.println("[2] 새로운 리스트 : " + newNumbers );
+    }// main end
+}// class end
