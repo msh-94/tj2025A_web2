53577b7aa3b84b5e49a01847ca23f2aeeabacf82
msh-94 <alstjdgh2003@naver.com>
2025-10-13 12:27:07 +0900

    xml 이용한 mybatis 삭제 개별조회 수정

--- src/main/java/example/day13/XmlController.java
+++ src/main/java/example/day13/XmlController.java
@@ -16,8 +16,10 @@ public class XmlController { // class start
     @PostMapping("") // http://localhost:8080/xml
     public ResponseEntity<?> save(@RequestBody StudentDto dto ){
         // < ? > : 제네릭타입에 ? 넣으면 모든 타입을 지칭한다 , 와일드카드
-        xmlMapper.save( dto );
-        return ResponseEntity.ok( true );
+        System.out.println("dto = " + dto); // soutp , save 실행전
+        xmlMapper.save( dto ); // ========= SQL 실행 ========
+        System.out.println("dto = " + dto); // soutp , save 실행후
+        return ResponseEntity.ok( dto ); // 제네릭 ? 이므로 모든 자료가 대입된다
     }// func end
 
     // [2] 전체조회
@@ -25,6 +27,27 @@ public class XmlController { // class start
     public ResponseEntity< ? > findAll(){
         List< StudentDto > list = xmlMapper.findAll();
         return ResponseEntity.ok(list);
+    }// func end
+
+    // [3] 개별 학생 조회
+    @GetMapping("/find")
+    public ResponseEntity< ? > find(@RequestParam int sno){
+        StudentDto dto = xmlMapper.find(sno);
+        return ResponseEntity.ok(dto);
+    }// func end
+
+    // [4] 개별 학생 삭제
+    @DeleteMapping("")
+    public ResponseEntity< ? > delete(@RequestParam int sno){
+        int result = xmlMapper.delete(sno);
+        return ResponseEntity.ok(result);
+    }// func end
+
+    // [5] 개별 학생 수정
+    @PutMapping("")
+    public ResponseEntity< ? > update(@RequestBody StudentDto dto){
+        int result = xmlMapper.update(dto);
+        return ResponseEntity.ok(result);
     }// func end
 
 }// class end
--- src/main/java/example/day13/XmlMapper.java
+++ src/main/java/example/day13/XmlMapper.java
@@ -1,6 +1,8 @@
 package example.day13; // 패키지명
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 import java.util.List;
 
@@ -11,9 +13,22 @@ public interface XmlMapper { // class start
     // Mybatis 에서 SQL 매핑하는 방법
     // 방법1 : 추상메소드 위에 @Insert("SQL") 작성 , 간단한 SQL CRUD 권장
     // 방법2 : 추상메소드를 매핑하는 XML 파일에서 SQL 작성 , 복잡한 SQL 권장
+
+    // [ 생성된 PK값 반환하는 방법 ]
+    // @Insert("INSERT INTO student(name , kor , math) values( #{name} , #{kor} , #{math} )")
+    // @Options( useGeneratedKeys = true , keyProperty = "sno" ) // 생성된 PK값 sno 필드에 매핑
     int save( StudentDto dto );
 
     // [2] 전체조회
     List<StudentDto> findAll();
+
+    // [3] 개별 학생 조회
+    StudentDto find(int sno);
+
+    // [4] 개별 학생 삭제
+    int delete(int sno);
+
+    // [5] 개별 학생 수정
+    int update( StudentDto dto );
 
 }// class end
