285fd10579d7a775372fbd7185187fd419ab1780
msh-94 <alstjdgh2003@naver.com>
2025-11-03 10:22:20 +0900

    jpa 설치 및 환경설정

---
+++ src/main/java/example2/day01/ExamEntity.java
@@ -0,0 +1,13 @@
+package example2.day01;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity // 해당 클래스에 엔티티임을 주입
+public class ExamEntity {
+    // * dto 처럼 데이터베이스에서 사용될 테이블과 속성(열) 일치
+    @Id // primary key
+    private int col1;
+    private String col2;
+    private double col3;
+}// class end
