579ad36f48e070ea01ae69b49a171711d59ce5b0
msh-94 <alstjdgh2003@naver.com>
2025-11-04 12:03:20 +0900

    JPA CRUD

--- src/main/java/example2/day02/GoodsController.java
+++ src/main/java/example2/day02/GoodsController.java
@@ -1,13 +1,46 @@
 package example2.day02;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController @RequestMapping("/api/goods")
 @RequiredArgsConstructor
 public class GoodsController {
 
     private final GoodsService goodsService;
+
+    // [1] 등록
+    @PostMapping // localhost:8080/api/goods
+    // { "gname" : "아메리카노" , "gprice" : "1500" , "gdesc" : "콜롬비아원두" }
+    public ResponseEntity<?> save(@RequestBody GoodsDto dto){
+        GoodsDto goodsDto = goodsService.save(dto);
+        return ResponseEntity.ok(goodsDto);
+    }// func end
+
+    // [2] 전체조회
+    @GetMapping // localhost:8080/api/goods
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.ok(goodsService.findAll());
+    }// func end
+
+    // [3] 개별조회
+    @GetMapping("/find") // localhost:8080/api/goods/find?gno=
+    public ResponseEntity<?> find(@RequestParam int gno){
+        return ResponseEntity.ok(goodsService.find(gno));
+    }// func end
+
+    // [4] 개별삭제
+    @DeleteMapping() // localhost:8080/api/goods?gno=
+    public ResponseEntity<?> delete(@RequestParam int gno){
+        return ResponseEntity.ok(goodsService.delete(gno));
+    }// func end
+
+    // [5] 개별수정
+    @PutMapping() // localhost:8080/api/goods
+    // { "gno" : "2" , "gname" : "헛개리카노" , "gprice" : "2000" , "gdesc" : "헛개수" }
+    public ResponseEntity<?> update(@RequestBody GoodsDto dto){
+        return ResponseEntity.ok(goodsService.update(dto));
+    }// func end
 
 } // class end
--- src/main/java/example2/day02/GoodsService.java
+++ src/main/java/example2/day02/GoodsService.java
@@ -4,10 +4,71 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service @RequiredArgsConstructor
 @Transactional
 public class GoodsService {
 
     private final GoodsRepository goodsRepository;
+
+    // [1] 등록
+    public GoodsDto save(GoodsDto dto){
+        GoodsEntity entity = dto.toEntity();
+        GoodsEntity saveEntity = goodsRepository.save(entity);
+        return saveEntity.toDto();
+    }// func end
+
+    // [2] 전체조회
+    public List<GoodsDto> findAll(){
+        List<GoodsEntity> entityList = goodsRepository.findAll();
+        // 방법1 for 문
+        //List<GoodsDto> dtoList = new ArrayList<>();
+        //for (GoodsEntity entity : entityList){
+        //    dtoList.add(entity.toDto());
+        //}// for end
+        // 방법2 스트림 API
+        List<GoodsDto> goodsDtoList = entityList.stream()
+                .map( GoodsEntity :: toDto ).collect(Collectors.toList());
+        return goodsDtoList;
+    }// func end
+
+    // [3] 개별조회
+    public GoodsDto find(int gno){
+        Optional<GoodsEntity> optional = goodsRepository.findById(gno);
+        if (optional.isPresent()){
+            GoodsEntity entity = optional.get();
+            return entity.toDto();
+        }// if end
+        return null;
+    }// func end
+
+    // [4] 개별삭제
+    public boolean delete(int gno){
+        goodsRepository.deleteById(gno);
+        Optional<GoodsEntity> optional = goodsRepository.findById(gno);
+        if (optional.isPresent()){
+            return false;
+        }// if end
+        return true;
+    }// func end
+
+    // [5] 개별수정
+    public GoodsDto update(GoodsDto dto){
+        Optional<GoodsEntity> optional = goodsRepository.findById(dto.getGno());
+        if (optional.isPresent()){
+            GoodsEntity goodsEntity = optional.get();
+            goodsEntity.setGname(dto.getGname());
+            goodsEntity.setGdesc(dto.getGdesc());
+            goodsEntity.setGdesc(dto.getGdesc());
+            return goodsEntity.toDto();
+        }else {
+            GoodsEntity goodsEntity = goodsRepository.save(dto.toEntity());
+            return goodsEntity.toDto();
+        }// if end
+    }// func end
 
 }// class end
