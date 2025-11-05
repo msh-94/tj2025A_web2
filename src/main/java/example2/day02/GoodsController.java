949d32e4632ac450b5477a1daf4ef9f4e9b1c340
msh-94 <alstjdgh2003@naver.com>
2025-11-05 12:14:44 +0900

    crossOrigin 추가

--- src/main/java/example2/day02/GoodsController.java
+++ src/main/java/example2/day02/GoodsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController @RequestMapping("/api/goods")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class GoodsController {
 
     private final GoodsService goodsService;
