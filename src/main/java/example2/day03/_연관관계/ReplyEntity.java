9f273f478de2cda69703442752abe152cb8c0149
msh-94 <alstjdgh2003@naver.com>
2025-11-06 12:06:43 +0900

    양방향 참조 엔티티 설계

---
+++ src/main/java/example2/day03/_연관관계/ReplyEntity.java
@@ -0,0 +1,22 @@
+package example2.day03._연관관계;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @Builder
+@Table( name = "ereply" )
+@NoArgsConstructor @AllArgsConstructor
+public class ReplyEntity {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private int rno; // 댓글번호
+    private String rcontent; // 댓글내용
+    // ** 단방향 연결 ** //
+    @ManyToOne( cascade = CascadeType.ALL , fetch = FetchType.LAZY )
+    @JoinColumn( name = "bno" ) // fk 필드명
+    private BoardEntity boardEntity;
+
+}// class end
