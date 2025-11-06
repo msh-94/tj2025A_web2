46ffd67d147ac4d24d4ecb077a712bdb0c28a0ad
msh-94 <alstjdgh2003@naver.com>
2025-11-06 12:08:50 +0900

    양방향 참조 엔티티 설계

--- src/main/resources/sql/webspring2.sql
+++ src/main/resources/sql/webspring2.sql
@@ -1,5 +1,9 @@
 DROP DATABASE IF EXISTS springweb2;
 CREATE DATABASE springweb2;
 USE springweb2;
-
+select * from goods;
 select * from books;
+select * from movie;
+select * from ecategory;
+select * from eboard;
+select * from ereply;
